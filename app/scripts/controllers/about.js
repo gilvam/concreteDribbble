696ef4f59c759eba56fe1f5ce39190da347822db
Gilvam Mourao <gilvamm@gmail.com>
2017-01-10 12:39:55 -0200

    commit inicial, yo angular inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name concreteDribbbleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the concreteDribbbleApp
+ */
+angular.module('concreteDribbbleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name concreteDribbbleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the concreteDribbbleApp
+ */
+angular.module('concreteDribbbleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
